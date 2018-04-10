ac855235977b5f433675beaf3154b6902ccfa5d1
Max Vagner <sydneymax@gmail.com>
2018-04-10 15:06:12 +1000

    Add task to copy file when content changes

--- gruntfile.js
+++ gruntfile.js
@@ -6,7 +6,8 @@ module.exports = function(grunt) {
 
     // Project configuration.
     var config = {
-        pkg: grunt.file.readJSON('package.json'),        
+        pkg: grunt.file.readJSON('package.json'),  
+
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -15,7 +16,33 @@ module.exports = function(grunt) {
                 src: 'src/<%= pkg.name %>.js',
                 dest: 'build/<%= pkg.name %>.min.js'
             }
-        } // end uglify
+        },
+
+        watch: {
+            scripts: {
+                files: ['C:/Source/CustApps/Wng.Admin/Codebase/Presentation/Wng.Admin.Presentation.BulkLoadToolExe/ExcelScenarios/NomadsScenarios.xlsx'],
+                tasks: ['copy'],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
+
+        copy: {
+            main: {
+                files: [
+                    // includes files within path 
+                    {
+                        expand: true,
+                        cwd: 'C:/Source/CustApps/Wng.Admin/Codebase/Presentation/Wng.Admin.Presentation.BulkLoadToolExe/ExcelScenarios/',
+                        src: ['NomadsScenarios.xlsx'],
+                        dest: 'C:/Source/CustApps/Wng.Admin/Codebase/Presentation/Wng.Admin.Presentation.BulkLoadToolGui/bin/LOC-WN/ExcelScenarios/',
+                        filter: 'isFile'
+                    },
+
+                ],
+            },
+        },
     }
 
     // Within this function we can initialize our configuration object.
@@ -23,7 +50,9 @@ module.exports = function(grunt) {
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['watch']);
 };
